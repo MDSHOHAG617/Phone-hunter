df49ddfc44bab816916cf8d7daa5a0555e4eadef
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-03-01 02:33:30 +0600

    Almost done!

--- js/index.js
+++ js/index.js
@@ -43,21 +43,40 @@ const loadSearch = () => {
         }
 
     }
-
     else {
         alert("Please Enter a valid Name (Brand)");
-
-
     }
 
 }
 
 const loadDetails = phoneId => {
     console.log(phoneId);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displsyDetails(data.data))
+}
 
-
-    // const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
-    // fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
+const displsyDetails = phoneDetail => {
+    // console.table(phoneDetail);
+    console.log(phoneDetail.image);
+    const phoneDetails = document.getElementById('phone-detailes');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    // if (`${phoneDetail.releaseDate}== "")` {
+    //     document.getElementById('realesedate').style.display = "block"
+    // }
+    div.innerHTML = `
+    <img  class="w-50 text-center mx-auto my-2" src="${phoneDetail.image}" class="card-img-top" alt="...">
+    <p class="mx-auto">Release Date: ${phoneDetail.releaseDate}</p>
+    <h5 class="card-title ms-4"> Model Name: ${phoneDetail.name}</h5>
+    <p class="p-2 ms-3 mx-auto "><span class="fw-bold">Main Features:</span> ${phoneDetail.mainFeatures.storage},${phoneDetail.mainFeatures.displaySize},${phoneDetail.mainFeatures.chipSet} </p>
+    <p class="ms-4 mx-auto "><span class="fw-bold">Sensors:</span> ${phoneDetail.mainFeatures.sensors} </p>
+    <p class="ms-3 p-2 mx-auto "><span class="fw-bold">Other's:</span><br>
+   <span class="fw-bold">WLAN:</span> ${phoneDetail.others.WLAN}, 
+   <span class="fw-bold">Bluetooth:</span> ${phoneDetail.others.Bluetooth},<span class="fw-bold"> GPS:</span> ${phoneDetail.others.GPS},<span class="fw-bold"> NFC:</span> ${phoneDetail.others.NFC},<span class="fw-bold"> Radio:</span> ${phoneDetail.others.Radio},<span class="fw-bold"> USB:</span> ${phoneDetail.others.USB} </p>
+ 
+    
+    `;
+    phoneDetails.appendChild(div);
 }
