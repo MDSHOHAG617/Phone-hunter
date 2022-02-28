d4d53d0ebf8e8ad4ae1236b6380ef75d93a600c4
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-02-28 23:56:30 +0600

    got phone Slug

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
 const loadSearch = () => {
     const searcInput = document.getElementById('searchInput');
     const input = searcInput.value;
-    // input.value = "";
+    searcInput.value = "";
     // console.log(input)
+    if (input == "") {
+        alert("no result found")
+    }
+    else if (input == "samsung" || input == "iphone" || input == "oppo") {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayResults(data.data.slice(0, 20)))
 
 
-    const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayResults(data.data.slice(0, 20)))
+        const displayResults = phones => {
+            // console.log(phones)
+
+            const searchResult = document.getElementById('searchResult');
+            searchResult.innerHTML = "";
+            for (const phone of phones) {
 
 
-    const displayResults = phones => {
-        // console.log(phones)
-        const searchResult = document.getElementById('searchResult');
-        for (const phone of phones) {
-            console.log(phone)
-            const div = document.createElement('div');
-            div.innerHTML = ` <div class="col">
+                const div = document.createElement('div');
+                div.innerHTML = ` <div class="col">
             <div class="card">
                 <img  class="w-50 text-center mx-auto mt-3" src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body d-flex justify-content-evenly align-items-center">
@@ -27,15 +32,32 @@ const loadSearch = () => {
                     <p class="card-text">${phone.slug}</p>
                     </div>
                     <div>
-                    <button type="button" class="btn btn-warning fw-bold text-dark">Details</button>
+                    <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-warning fw-bold text-dark">Details</button>
                     </div>
                 </div>
             </div>
         </div>`
 
-            searchResult.appendChild(div)
+                searchResult.appendChild(div)
+            }
         }
+
+    }
+
+    else {
+        alert("Please Enter a valid Name (Brand)");
+
+
     }
 
 }
 
+const loadDetails = phoneId => {
+    console.log(phoneId);
+
+
+    // const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+}
