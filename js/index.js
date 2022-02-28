3600f1ed1be2fc3decb00aab53cbfd96c22b27c3
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-02-28 22:15:07 +0600

    main data show

--- js/index.js
+++ js/index.js
@@ -8,10 +8,34 @@ const loadSearch = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${input}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.slice(0, 20)))
+        .then(data => displayResults(data.data.slice(0, 20)))
 
 
+    const displayResults = phones => {
+        // console.log(phones)
+        const searchResult = document.getElementById('searchResult');
+        for (const phone of phones) {
+            console.log(phone)
+            const div = document.createElement('div');
+            div.innerHTML = ` <div class="col">
+            <div class="card">
+                <img  class="w-50 text-center mx-auto mt-3" src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body d-flex justify-content-evenly align-items-center">
+                    <div>
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <h5 class="card-title">${phone.brand}</h5>
+                    <p class="card-text">${phone.slug}</p>
+                    </div>
+                    <div>
+                    <button type="button" class="btn btn-warning fw-bold text-dark">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>`
 
+            searchResult.appendChild(div)
+        }
+    }
 
 }
 
