0515d1d1870721604eef692a870648837eb5455c
MDSHOHAG617 <95575228+MDSHOHAG617@users.noreply.github.com>
2022-03-02 01:07:16 +0600

    Alhamdulillah done!

--- js/index.js
+++ js/index.js
@@ -15,12 +15,14 @@ const loadSearch = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => displayResults(data.data.slice(0, 20)))
-
         /* Display Search Results */
         const displayResults = phones => {
             // console.log(phones)
+            /* Clearing Displaying results */
             const searchResult = document.getElementById('searchResult');
             searchResult.innerHTML = "";
+            const phoneDetails = document.getElementById('phone-detailes');
+            phoneDetails.innerHTML = "";
             for (const phone of phones) {
                 const div = document.createElement('div');
                 div.innerHTML = `
@@ -45,35 +47,27 @@ const loadSearch = () => {
     }
     /* Chking Errors */
     else {
-        error.innerText = "please enter a valid Brand name";
+        error.innerText = "No resuls found";
     }
 }
 /* Catching Phones Id number */
 const loadDetails = phoneId => {
-    console.log(phoneId);
+    // console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
         .then(data => displsyDetails(data.data))
 }
-
+/* Displaying Phones Details */
 const displsyDetails = phoneDetail => {
-    console.table(phoneDetail);
-    console.log(phoneDetail.image);
     const phoneDetails = document.getElementById('phone-detailes');
+    phoneDetail.innerHTML = "";
     const div = document.createElement('div');
     div.classList.add('card');
-
-
-
-    // if (`${phoneDetail.releaseDate}== "")` {
-    //     document.getElementById('realesedate').style.display = "block"
-    // }
     div.innerHTML = `<img  class="w-50 text-center mx-auto my-2" src="${phoneDetail.image}" class="card-img-top" alt="${phoneDetail.name || "No data found"}">
-    <p class="mx-auto mx-2">Release Date: ${phoneDetail.releaseDate || "No data found"}</p>
-   
+    <p class="mx-auto mx-2">Release Date: ${phoneDetail.releaseDate || "No realease date found"}</p>
     <h5 class="card-title ms-4"> Model Name: ${phoneDetail.name || "No data found"}</h5>
-    <p class="p-2 ms-3"><span class="fw-bold">Main Features:</span> ${phoneDetail.mainFeatures.storage},${phoneDetail.mainFeatures.displaySize},${phoneDetail.mainFeatures.chipSet} </p>
+    <p class="p-2 ms-3"><span class="fw-bold">Main Features:</span> ${phoneDetail.mainFeatures.storage},${phoneDetail.mainFeatures.displaySize}${phoneDetail.mainFeatures.chipSet} </p>
     <p class="ms-4  "><span class="fw-bold">Sensors:</span> ${phoneDetail.mainFeatures.sensors || "No data found"}</p>
     <p class="ms-3 p-2 mx-auto "><span class="fw-bold">Other's:</span><br>
    <span class="fw-bold">WLAN:</span> ${phoneDetail.others?.WLAN || "No data found"}, 
@@ -82,11 +76,8 @@ const displsyDetails = phoneDetail => {
    <span class="fw-bold"> NFC:</span> ${phoneDetail.others?.NFC || "No data found"},
    <span class="fw-bold"> Radio:</span> ${phoneDetail.others?.Radio || "No data found"},
    <span class="fw-bold"> USB:</span> ${phoneDetail.others?.USB || "No data found"} </p>
- 
     `;
-
     phoneDetails.appendChild(div);
-
 }
 
 
